7f6a5b09faca5b7c50604c970121245d9f7fb854
Arnau Espin <arnauespin@gmail.com>
2022-05-09 17:20:34 +0200

    .

--- components/CreateCM/Form.tsx
+++ components/CreateCM/Form.tsx
@@ -37,7 +37,6 @@ const Form: FC<{
   const { cache, uploadCache } = useUploadCache();
 
   const initialState = {
-    
     price: updateCandyMachine
       ? new BN(fetchedValues.price).toNumber() / LAMPORTS_PER_SOL
       : 0,
