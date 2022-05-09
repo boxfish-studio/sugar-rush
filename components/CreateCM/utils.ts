1144dbf4aa87927642efeac33efdf083d0b7b08c
Arnau Espin <arnauespin@gmail.com>
2022-05-09 14:17:50 +0200

    fix utils parse date functions

--- components/CreateCM/utils.ts
+++ components/CreateCM/utils.ts
@@ -7,49 +7,49 @@ import { BN } from '@project-serum/anchor';
  * @returns {string} time parsed to UCT
  */
 export function parseDateToUTC(dateTime: string, time: string): string {
-    let UTCDate: string[] | string = new Date(dateTime)
-      .toDateString()
-      .slice(4)
-      .split(" ");
-    const _temp = UTCDate[0];
-    UTCDate[0] = UTCDate[1];
-    UTCDate[1] = _temp;
-    UTCDate = UTCDate.join(".").replaceAll(".", " ");
-  
-    const UTCTime = `${time}:00 GMT`;
-  
-    return `${UTCDate} ${UTCTime}`;
+  let UTCDate: string[] | string = new Date(dateTime)
+    .toDateString()
+    .slice(4)
+    .split(' ');
+  const _temp = UTCDate[0];
+  UTCDate[0] = UTCDate[1];
+  UTCDate[1] = _temp;
+  UTCDate = UTCDate.join('.').replaceAll('.', ' ');
+
+  const UTCTime = `${time}:00 GMT`;
+
+  return `${UTCDate} ${UTCTime}`;
+}
+
+export function parseDateFromDateBN(date: BN) {
+  let year = new Date(new BN(date).toNumber() * 1000).getFullYear();
+  let month = new Date(new BN(date).toNumber() * 1000).getMonth().toString();
+  let day = new Date(new BN(date).toNumber() * 1000).getDate().toString();
+
+  if (month.length === 1) {
+    month = `0${month}`;
   }
 
-  export function parseDateFromDateBN(date: BN) {
-    return `${new Date(new BN(date).toNumber() * 1000).getFullYear()}-${
-      new Date(new BN(date).toNumber() * 1000).getMonth().toString().length == 1
-        ? '0'.concat(
-            new Date(new BN(date).toNumber() * 1000).getMonth().toString()
-          )
-        : new Date(new BN(date).toNumber() * 1000).getMonth().toString()
-    }-${
-      new Date(new BN(date).toNumber() * 1000).getDate().toString().length == 1
-        ? '0'.concat(
-            new Date(new BN(date).toNumber() * 1000).getDate().toString()
-          )
-        : new Date(new BN(date).toNumber() * 1000).getDate().toString()
-    }`;
+  if (day.length === 1) {
+    day = `0${day}`;
   }
-  
-  export function parseTimeFromDateBN(date: BN) {
-    return `${
-      new Date(new BN(date).toNumber() * 1000).getUTCHours().toString().length ==
-      1
-        ? '0'.concat(
-            new Date(new BN(date).toNumber() * 1000).getUTCHours().toString()
-          )
-        : new Date(new BN(date).toNumber() * 1000).getUTCHours().toString()
-    }:${
-      new Date(new BN(date).toNumber() * 1000).getMinutes().toString().length == 1
-        ? '0'.concat(
-            new Date(new BN(date).toNumber() * 1000).getMinutes().toString()
-          )
-        : new Date(new BN(date).toNumber() * 1000).getMinutes().toString()
-    }`;
+  return `${year}-${month}-${day}`;
+}
+
+export function parseTimeFromDateBN(date: BN) {
+  let hours = new Date(new BN(date).toNumber() * 1000).getUTCHours().toString();
+
+  if (hours.length === 1) {
+    hours = `0${hours}`;
   }
+
+  let minutes = new Date(new BN(date).toNumber() * 1000)
+    .getMinutes()
+    .toString();
+
+  if (minutes.length === 1) {
+    minutes = `0${minutes}`;
+  }
+
+  return `${hours}:${minutes}`;
+}
