9cccfc903392ca026bdce88a3b211dec43dbba82
Arnau Espin <arnauespin@gmail.com>
2022-05-10 14:09:30 +0200

    Feat: add search bar to filter cm list

--- hooks/index.ts
+++ hooks/index.ts
@@ -2,3 +2,4 @@ export {default as useForm} from './useForm';
 export {default as useUploadFiles} from './useUploadFiles';
 export {default as useRPC} from './useRPC';
 export {default as useUploadCache} from './useUploadCache';
+export {default as useSearchBar} from './useSearchBar';
