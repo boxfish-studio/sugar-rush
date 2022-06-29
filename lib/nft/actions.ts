a93d430f433f61ca1f10a55dad8b2276717d5ad4
cpl121 <cpeon@boxfish.studio>
2022-06-29 09:13:30 +0200

    Move findAllByCandyMachine to lib/nft/actions

---
+++ lib/nft/actions.ts
@@ -0,0 +1,23 @@
+import { Metaplex } from '@metaplex-foundation/js'
+import { useConnection } from '@solana/wallet-adapter-react'
+import { PublicKey } from '@solana/web3.js'
+import { Nft } from './interfaces'
+
+export async function getAllNftsByCM(account: string | string[]) {
+    const { connection } = useConnection()
+
+    const metaplex = new Metaplex(connection)
+    const nftsAddresses = await metaplex.nfts().findAllByCandyMachine(new PublicKey(account))
+
+    const nfts: Nft[] = []
+    for (let i = 0; i < nfts.length; i++) {
+        let fetchData = await fetch(nftsAddresses[i].uri)
+        let nftData = await fetchData.json()
+        let nft: Nft = {
+            name: nfts[i].name,
+            imageLink: nftData.image,
+        }
+        nfts.push(nft)
+    }
+    return nfts
+}
