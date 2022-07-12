beadd463998881a767bf2c9e357de6f9c435004d
Jean Carlo Noguera <jcnoguera@boxfish.studio>
2022-07-12 15:47:50 +0200

    refactor: update top actions and search bar behavior

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -1,17 +1,21 @@
+import { TextInput } from '@primer/react'
 import { Dispatch, SetStateAction, FC } from 'react'
 import { SetterOrUpdater } from 'recoil'
+import { SearchIcon } from '@primer/octicons-react'
 
 const SearchBar: FC<{
     searchValue: string
     setSearchValue: SetterOrUpdater<string> | Dispatch<SetStateAction<string>>
 }> = ({ searchValue, setSearchValue }) => {
     return (
-        <input
-            className='border py-1 rounded-lg searchbox-candymachine f5'
+        <TextInput
+            icon={SearchIcon}
+            className='border py-1 rounded-lg searchbox-candymachine f5 pl-2'
             type='search'
             placeholder='Search candy machine'
             onChange={({ target }) => setSearchValue(target.value)}
             value={searchValue}
+            sx={{ '> input': { padding: '0' }, '> svg': { width: '15px', height: '15px' } }}
         />
     )
 }
--- components/TopActions.tsx
+++ components/TopActions.tsx
@@ -1,15 +1,32 @@
 import { FC } from 'react'
+import { useRouter } from 'next/router'
 import SearchBar from './SearchBar'
 import { useRecoilState } from 'recoil'
 import { candyMachineSearchState } from 'lib/recoil/atoms'
+import { useWallet } from '@solana/wallet-adapter-react'
+import { Button } from '@primer/react'
+import { LinkExternalIcon } from '@primer/octicons-react'
 
 const TopActions: FC = () => {
     const [searchValue, setSearchValue] = useRecoilState(candyMachineSearchState)
+    const router = useRouter()
+    const { publicKey } = useWallet()
 
-    return (
-        <div className='d-flex flex-justify-end top-actions-bar d-flex flex-row pt-8'>
-            <SearchBar searchValue={searchValue} setSearchValue={setSearchValue} />
-        </div>
-    )
+    if (publicKey) {
+        return (
+            <div className='d-flex flex-justify-end top-actions-bar d-flex flex-row pt-8'>
+                {router.pathname === '/' ? (
+                    <SearchBar searchValue={searchValue} setSearchValue={setSearchValue} />
+                ) : (
+                    <>
+                        <Button variant='danger'>Delete</Button>
+                        <Button variant='outline'>Verify</Button>
+                        <Button leadingIcon={LinkExternalIcon}>View in Solscan</Button>
+                    </>
+                )}
+            </div>
+        )
+    }
+    return null
 }
 export default TopActions
