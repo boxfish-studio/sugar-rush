013d16a42b9fc0c423d69e77ac9b45c9dea2b9a4
Jean Carlo Noguera <jcnoguera@boxfish.studio>
2022-07-12 16:42:06 +0200

    feat: remove connected condition from top-actions

--- components/TopActions.tsx
+++ components/TopActions.tsx
@@ -3,30 +3,26 @@ import { useRouter } from 'next/router'
 import SearchBar from './SearchBar'
 import { useRecoilState } from 'recoil'
 import { candyMachineSearchState } from 'lib/recoil/atoms'
-import { useWallet } from '@solana/wallet-adapter-react'
 import { Button } from '@primer/react'
 import { LinkExternalIcon } from '@primer/octicons-react'
 
 const TopActions: FC = () => {
     const [searchValue, setSearchValue] = useRecoilState(candyMachineSearchState)
-    const router = useRouter()
-    const { publicKey } = useWallet()
+    const { pathname } = useRouter()
 
-    if (publicKey) {
-        return (
-            <div className='d-flex flex-justify-end top-actions-bar d-flex flex-row pt-8'>
-                {router.pathname === '/' ? (
-                    <SearchBar searchValue={searchValue} setSearchValue={setSearchValue} />
-                ) : (
-                    <>
-                        <Button variant='danger'>Delete</Button>
-                        <Button variant='outline'>Verify</Button>
-                        <Button leadingIcon={LinkExternalIcon}>View in Solscan</Button>
-                    </>
-                )}
-            </div>
-        )
-    }
-    return null
+    return (
+        <div className='d-flex flex-justify-end top-actions-bar d-flex flex-row pt-8'>
+            {pathname === '/' ? (
+                <SearchBar searchValue={searchValue} setSearchValue={setSearchValue} />
+            ) : (
+                <>
+                    <Button variant='danger'>Delete</Button>
+                    <Button variant='outline'>Verify</Button>
+                    <Button leadingIcon={LinkExternalIcon}>View in Solscan</Button>
+                </>
+            )}
+        </div>
+    )
 }
+
 export default TopActions
