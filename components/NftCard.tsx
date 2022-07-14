13b1def31556b8557648e715692e0be3204cda80
cpl121 <cpeon@boxfish.studio>
2022-07-14 16:39:27 +0200

    Create NFT card component and create parent component to nft detail

---
+++ components/NftCard.tsx
@@ -0,0 +1,34 @@
+import { useConnection } from '@solana/wallet-adapter-react'
+import { Link, Button, Text, IconButton } from '@primer/react'
+import { FC } from 'react'
+import { LinkExternalIcon } from '@primer/octicons-react'
+
+const NftCard: FC<{ title: string; imageLink: string; hash?: string }> = ({ title, imageLink, hash }) => {
+    const { connection } = useConnection()
+    return (
+        <div
+            className='d-flex flex-column flex-justify-center flex-items-center border rounded-3 gap-2 py-2'
+            style={{ width: '185px' }}
+        >
+            <Text as='p' fontWeight='bold'>
+                {title}
+            </Text>
+            <img className='border rounded-3' style={{ height: '168px', width: '168px' }} src={imageLink} />
+            {hash && (
+                <Link
+                    target='_blank'
+                    rel='noopener noreferrer'
+                    href={`https://solscan.io/account/${hash}?${
+                        connection.rpcEndpoint.includes('devnet') ? '?cluster=devnet' : ''
+                    }`}
+                >
+                    <Button leadingIcon={LinkExternalIcon} variant='invisible'>
+                        View in solscan
+                    </Button>
+                </Link>
+            )}
+        </div>
+    )
+}
+
+export default NftCard
