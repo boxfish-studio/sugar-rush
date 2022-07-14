fa2da4b60c109fabca0a2779dd2cd8da24423e81
evavirseda <evirseda@boxfish.studio>
2022-07-14 11:15:31 +0200

    Add link to solscan

--- components/cards/CandyMachineTile.tsx
+++ components/cards/CandyMachineTile.tsx
@@ -1,11 +1,13 @@
-import { Button } from '@primer/react'
-import Link from 'next/link'
 import { FC } from 'react'
+import Link from 'next/link'
+import { useConnection } from '@solana/wallet-adapter-react'
+import { Button } from '@primer/react'
 import { LinkExternalIcon } from '@primer/octicons-react'
 
 const CandyMachineTile: FC<{
     account: string
 }> = ({ account }) => {
+    const { connection } = useConnection()
     return (
         <>
             <div
@@ -16,8 +18,14 @@ const CandyMachineTile: FC<{
                     {account}
                 </span>
                 <div className='d-flex mt-2 mt-md-0 '>
-                    <Link href={`/${account}`}>
-                        <Button leadingIcon={LinkExternalIcon}>View in Solscan</Button>
+                    <Link
+                        href={`https://solscan.io/account/${account}?${
+                            connection.rpcEndpoint.includes('devnet') ? '?cluster=devnet' : ''
+                        }`}
+                    >
+                        <a target='_blank'>
+                            <Button leadingIcon={LinkExternalIcon}>View in Solscan</Button>
+                        </a>
                     </Link>
                     <Link href={`/${account}`}>
                         <Button variant='outline' sx={{ ml: '16px' }}>
