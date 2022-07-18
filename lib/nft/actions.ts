462a5020bd72a915fe75e1b929cb96f1e5220cc8
cpl121 <cpeon@boxfish.studio>
2022-07-18 12:35:44 +0200

    Remove for loop

--- lib/nft/actions.ts
+++ lib/nft/actions.ts
@@ -7,34 +7,34 @@ export async function getAllNftsByCM(candyMachineAccount: string | string[], con
     try {
         const metaplex = new Metaplex(connection)
         const nftsAddresses = await metaplex.nfts().findAllByCandyMachine(new PublicKey(candyMachineAccount as string))
-        const nfts: Nft[] = []
-        for (let i = 0; i < nftsAddresses.length; i++) {
-            let nft: Nft
-            if (nftsAddresses[i].uri) {
-                let fetchData = await fetch(nftsAddresses[i].uri)
-                let nftData = await fetchData.json()
-                nft = {
-                    name: nftData.name,
-                    image: nftData.image,
-                    description: nftData.description,
-                    animation_url: nftData.animation_url,
-                    external_url: nftData.external_url,
-                    symbol: nftData.symbol,
-                    collection: nftData.collection,
-                    attributes: nftData.attributes,
-                    properties: nftData.properties,
-                    category: nftData.category,
-                    creators: nftData.creators,
-                    seller_fee_basis_points: nftData.seller_fee_basis_points,
-                }
-            } else {
-                nft = {
-                    name: nftsAddresses[i].name,
-                    image: '',
-                }
-            }
-            nfts.push(nft)
-        }
+        const nfts: Nft[] =
+            (await Promise.all(
+                nftsAddresses?.map(async (nft) => {
+                    if (nft.uri) {
+                        let fetchData = await fetch(nft.uri)
+                        let nftData = await fetchData.json()
+                        return {
+                            name: nftData.name,
+                            image: nftData.image,
+                            description: nftData.description,
+                            animation_url: nftData.animation_url,
+                            external_url: nftData.external_url,
+                            symbol: nftData.symbol,
+                            collection: nftData.collection,
+                            attributes: nftData.attributes,
+                            properties: nftData.properties,
+                            category: nftData.category,
+                            creators: nftData.creators,
+                            seller_fee_basis_points: nftData.seller_fee_basis_points,
+                        }
+                    } else {
+                        return {
+                            name: nft.name,
+                            image: '',
+                        }
+                    }
+                })
+            )) ?? []
         return nfts
     } catch (error) {
         console.log(error)
