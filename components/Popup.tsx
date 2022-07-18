4cdfc76f61cbfcd71c3d6ede0cb85f4778bd799f
evavirseda <evirseda@boxfish.studio>
2022-07-18 18:30:11 +0200

    Update reusable Popup

--- components/Popup.tsx
+++ components/Popup.tsx
@@ -1,26 +1,47 @@
-import { Dispatch, FC, SetStateAction } from 'react'
-import { Box } from '@primer/react'
-import { Dialog } from '@primer/react/drafts'
+import { FC } from 'react'
+import Image from 'next/image'
+import { Box, Button } from '@primer/react'
 
 const Popup: FC<{
-    setIsOpen: Dispatch<SetStateAction<boolean>>
     children: any
     title: string
-    onClick: () => void
-}> = ({ setIsOpen, children, title, onClick }) => {
+    onClose: () => void
+    size: 'small' | 'large'
+}> = ({ children, title, onClose, size }) => {
     return (
-        <>
-            <Dialog
-                title={title}
-                onClose={() => setIsOpen(false)}
-                aria-labelledby='label'
-                footerButtons={[{ content: 'Ok', onClick: onClick }]}
-                height='large'
-                width='xlarge'
+        <div
+            className='position-absolute width-full height-full top-0 left-0'
+            style={{ background: 'rgba(0, 0, 0, 0.5)' }}
+        >
+            <Box
+                className='position-fixed color-bg-inset rounded-2'
+                sx={{
+                    zIndex: 10,
+                    width: '100%',
+                    maxWidth: '60vh',
+                    height: '100%',
+                    maxHeight: size === 'large' ? ['550px', '750px'] : ['50%'],
+                    top: '50%',
+                    left: '50%',
+                    transform: 'translate(-50%, -50%)',
+                }}
             >
-                <Box p={3}>{children}</Box>
-            </Dialog>
-        </>
+                <div className='p-3 p-md-5 d-flex flex-column height-full flex-justify-between'>
+                    <div className='d-flex flex-justify-between text-bold'>
+                        <h3>{title}</h3>
+                        <Image
+                            src='/close.svg'
+                            alt='close'
+                            width={22}
+                            height={22}
+                            onClick={onClose}
+                            style={{ cursor: 'pointer' }}
+                        />
+                    </div>
+                    <div className='mt-5'>{children}</div>
+                </div>
+            </Box>
+        </div>
     )
 }
 
