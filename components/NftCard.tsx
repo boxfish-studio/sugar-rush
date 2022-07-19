cea4137b15e773921c4e4c440c2eb5131512d3ce
cpl121 <cpeon@boxfish.studio>
2022-07-19 13:02:11 +0200

    primer css changes

--- components/NftCard.tsx
+++ components/NftCard.tsx
@@ -18,7 +18,7 @@ const NftCard: FC<{ title: string; imageLink: string; hash?: string }> = ({ titl
                 className='border rounded-3 mb-2 overflow-hidden d-flex flex-items-center flex-justify-center'
                 style={{ height: '168px', width: '168px' }}
             >
-                <div style={{ width: '100%', height: '100%', position: 'relative' }}>
+                <div className='width-full height-full position-relative'>
                     <Image alt={title} src={imageLink} layout='fill' objectFit='cover' />
                 </div>
             </div>
