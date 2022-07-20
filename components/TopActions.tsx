1c1164fdb350fcc59400815d2afd78e6b8845175
evavirseda <evirseda@boxfish.studio>
2022-07-20 15:37:30 +0200

    Add link to solscan

--- components/TopActions.tsx
+++ components/TopActions.tsx
@@ -3,20 +3,22 @@ import { useRouter } from 'next/router'
 import { SearchBar, RefreshButton } from './'
 import { useRecoilState, useSetRecoilState } from 'recoil'
 import { candyMachineSearchState, candyMachinesState } from 'lib/recoil-store/atoms'
-import { Button } from '@primer/react'
+import { Button, Link } from '@primer/react'
 import { LinkExternalIcon } from '@primer/octicons-react'
-import { useWallet } from '@solana/wallet-adapter-react'
+import { useWallet, useConnection } from '@solana/wallet-adapter-react'
 import { useRPC } from 'hooks'
 import { fetchCandyMachineAccounts } from 'lib/utils'
 import { Popup, CreateCandyMachine } from 'components'
 
 const TopActions: FC = () => {
     const [searchValue, setSearchValue] = useRecoilState(candyMachineSearchState)
-    const { pathname } = useRouter()
+    const { pathname, query } = useRouter()
     const [isLoading, setIsLoading] = useState(false)
     const { publicKey } = useWallet()
     const { rpcEndpoint } = useRPC()
     const [isOpen, setIsOpen] = useState(false)
+    const candyMachineAccount = query?.id
+    const { connection } = useConnection()
 
     const setCandyMachines = useSetRecoilState(candyMachinesState)
 
@@ -54,7 +56,17 @@ const TopActions: FC = () => {
                 <>
                     <Button variant='danger'>Delete</Button>
                     <Button variant='outline'>Verify</Button>
-                    <Button leadingIcon={LinkExternalIcon}>View in Solscan</Button>
+                    <Button leadingIcon={LinkExternalIcon}>
+                        <Link
+                            target='_blank'
+                            href={`https://solscan.io/account/${candyMachineAccount}?${
+                                connection.rpcEndpoint.includes('devnet') ? '?cluster=devnet' : ''
+                            }`}
+                            sx={{ textDecoration: 'none', color: '#24292F' }}
+                        >
+                            View in Solscan
+                        </Link>
+                    </Button>
                 </>
             )}
         </div>
