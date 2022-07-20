010c77341639dede2ffea88b0f0e8b10ea7158b7
cpl121 <100352899+cpl121@users.noreply.github.com>
2022-07-20 16:15:13 +0200

    Feat: Add mint button (#143)
    * Add mint button
    
    * some buttons in nft card
    
    * add another condition in nft card

--- components/NftCard.tsx
+++ components/NftCard.tsx
@@ -1,41 +1,66 @@
 import { useConnection } from '@solana/wallet-adapter-react'
-import { Link, Button, Text, IconButton } from '@primer/react'
+import { Link, Text, Button, Spinner } from '@primer/react'
 import { FC } from 'react'
 import { LinkExternalIcon } from '@primer/octicons-react'
 import Image from 'next/image'
+import { VariantType } from '@primer/react/lib/Button/types'
 
-const NftCard: FC<{ title: string; imageLink: string; hash?: string }> = ({ title, imageLink, hash }) => {
+const NftCard: FC<{
+    title: string
+    imageLink?: string
+    buttons?: {
+        text: string
+        as: 'link' | 'button'
+        hash?: string
+        isLoading?: boolean
+        variant: VariantType
+        onClick?: () => void
+    }[]
+}> = ({ title, imageLink, buttons }) => {
     const { connection } = useConnection()
-    const image = imageLink !== '' ? imageLink : '/default-image.png'
     return (
         <div
-            className='d-flex flex-column flex-justify-center flex-items-center border rounded-3 gap-2 py-2'
+            className='d-flex flex-column flex-justify-center flex-items-center border rounded-3 py-2'
             style={{ width: '185px' }}
         >
             <Text as='p' fontWeight='bold'>
                 {title}
             </Text>
             <div
-                className='border rounded-3 mb-2 overflow-hidden d-flex flex-items-center flex-justify-center'
+                className='border color-bg-inset rounded-3 mb-2 overflow-hidden d-flex flex-items-center flex-justify-center'
                 style={{ height: '168px', width: '168px' }}
             >
                 <div className='width-full height-full position-relative'>
-                    <Image alt={title} src={imageLink} layout='fill' objectFit='cover' />
+                    {imageLink?.length && <Image alt={title} src={imageLink} layout='fill' objectFit='cover' />}
                 </div>
             </div>
-            {hash && (
-                <Link
-                    target='_blank'
-                    rel='noopener noreferrer'
-                    href={`https://solscan.io/token/${hash}${
-                        connection.rpcEndpoint.includes('devnet') ? '?cluster=devnet' : ''
-                    }`}
-                >
-                    <Button leadingIcon={LinkExternalIcon} variant='invisible'>
-                        View in solscan
+
+            {buttons
+                ?.filter((btn) => btn.as === 'button')
+                .map((btn, i) => (
+                    <Button variant={btn.variant} onClick={() => btn.onClick && btn.onClick()} key={i}>
+                        <div className={'d-flex flex-row flex-justify-center flex-items-center'}>
+                            {btn.isLoading && <Spinner sx={{ mr: 2 }} size='small' />}
+                            {btn.text}
+                        </div>
                     </Button>
-                </Link>
-            )}
+                ))}
+            {buttons
+                ?.filter((btn) => btn.as === 'link')
+                .map((btn, i) => (
+                    <Link
+                        key={i}
+                        target='_blank'
+                        rel='noopener noreferrer'
+                        href={`https://solscan.io/account/${btn.hash}?${
+                            connection.rpcEndpoint.includes('devnet') ? '?cluster=devnet' : ''
+                        }`}
+                    >
+                        <Button leadingIcon={LinkExternalIcon} variant={btn.variant}>
+                            {btn.text}
+                        </Button>
+                    </Link>
+                ))}
         </div>
     )
 }
