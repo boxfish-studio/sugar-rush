8669bca84552547db99db1dd0e5907dd167df11d
Arnau Espin <arnauespin@gmail.com>
2022-07-21 10:47:54 +0200

    style: add cursor default to network dropdown

--- components/NetworkSelector.tsx
+++ components/NetworkSelector.tsx
@@ -20,7 +20,7 @@ const NetworkSelector: FC = () => {
         }
     }, [])
     return (
-        <div className='pr-4'>
+        <div className='pr-4 cursor-default'>
             <details
                 id='details'
                 className='dropdown details-reset details-overlay d-inline-block cursor-pointer'
