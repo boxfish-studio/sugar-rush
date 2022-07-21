63c637276f554dbbccb935f9a0eb21f381a9a64d
Arnau Espin <arnauespin@gmail.com>
2022-07-21 11:18:53 +0200

    feat: remove warning icon and decrease text size

--- components/NetworkTps.tsx
+++ components/NetworkTps.tsx
@@ -1,8 +1,6 @@
 import { FC, useCallback, useEffect, useMemo, useState } from 'react'
 import { useRPC } from 'hooks'
 
-const MIN_TPS = 2500
-
 const NetworkTps: FC = () => {
     const { connection } = useRPC()
     const [averageTps, setAverageTps] = useState(0)
@@ -25,11 +23,10 @@ const NetworkTps: FC = () => {
     }, [getNetworkPerformance, connection])
 
     const tps = useMemo(() => averageTps.toFixed() ?? 'N/A', [averageTps])
-    const networkPerformanceText = useMemo(() => (averageTps > MIN_TPS ? '' : '⚠️'), [averageTps])
     if (averageTps === 0) return <></>
     return (
-        <div className='d-flex gap-2 flex-justify-center flex-items-center flex-content-center pr-4 f4 cursor-default'>
-            <p className='m-0'>{networkPerformanceText}</p> <p className='m-0 pl-2'>{tps}</p>
+        <div className='d-flex gap-2 flex-justify-center flex-items-center flex-content-center pr-4 f5 cursor-default'>
+            <p className='m-0 pl-2'>{tps}</p>
             <p className='m-0 pl-2'>TPS</p>
         </div>
     )
