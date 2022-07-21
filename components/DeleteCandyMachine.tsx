4ff23df20d0e55b80c24f0e8ccf0bb8f22d75a63
evavirseda <evirseda@boxfish.studio>
2022-07-21 14:06:08 +0200

    Refine delete popup

--- components/DeleteCandyMachine.tsx
+++ components/DeleteCandyMachine.tsx
@@ -31,66 +31,67 @@ const DeleteCandyMachine: FC<{
 
     return (
         <>
-            <div className='d-flex flex-column flex-justify-between'>
+            <div className='d-flex flex-column flex-justify-between height-full'>
                 <div>
                     <Text as='p'>Are you sure you want to delete candy machine</Text>
                     <Text as='p' className='wb-break-all'>
                         {candyMachineAccount}
                     </Text>
-                    {!isDeleting && status.message && (
-                        <>
-                            <span
-                                className={`border ${
-                                    status.error
-                                        ? 'color-fg-danger color-bg-danger color-border-danger'
-                                        : 'color-fg-success color-bg-success color-border-success'
-                                } rounded-2 p-2`}
-                            >
-                                {status.message}
-                            </span>
-                            {!status.error && (
-                                <div className='mt-3'>
-                                    <ExplorerLinks
-                                        type='transaction'
-                                        value={transaction}
-                                        connection={connection}
-                                        text={'Check tx'}
-                                    />
-                                </div>
-                            )}
-                        </>
-                    )}
                 </div>
-            </div>
-            {!isDeleting && !transaction && (
-                <Button
-                    className={`width-full ${status.message ? 'mt-3' : 'mt-5'}`}
-                    variant='danger'
-                    state='rest'
-                    size='medium'
-                    onClick={() => deleteCM()}
-                >
-                    Delete candy machine
-                </Button>
-            )}
-            {!isDeleting && !!transaction && (
-                <Button
-                    className={`width-full mt-3`}
-                    variant='outline'
-                    state='rest'
-                    size='medium'
-                    onClick={() => router.push('/')}
-                >
-                    Go Home
-                </Button>
-            )}
-            {isDeleting && (
-                <>
-                    <Button className='width-full mt-4' isLoading disabled size='large'>
-                        Deleting Candy Machine... <Spinner size='small' />
+                {!isDeleting && status.message && (
+                    <>
+                        <span
+                            className={`border ${
+                                status.error
+                                    ? 'color-fg-danger color-bg-danger color-border-danger'
+                                    : 'color-fg-success color-bg-success color-border-success'
+                            } rounded-2 p-2`}
+                        >
+                            {status.message}
+                        </span>
+                        {!status.error && (
+                            <div className='mt-3'>
+                                <ExplorerLinks
+                                    type='transaction'
+                                    value={transaction}
+                                    connection={connection}
+                                    text={'Check tx'}
+                                />
+                            </div>
+                        )}
+                    </>
+                )}
+
+                {!isDeleting && !transaction && (
+                    <Button
+                        className={`width-full ${status.message ? 'mt-3' : 'mt-5'}`}
+                        variant='danger'
+                        state='rest'
+                        size='medium'
+                        onClick={() => deleteCM()}
+                    >
+                        Delete candy machine
                     </Button>
-                </>
-            )}
+                )}
+                {!isDeleting && !!transaction && (
+                    <Button
+                        className='width-full mt-3'
+                        variant='outline'
+                        state='rest'
+                        size='medium'
+                        onClick={() => router.push('/')}
+                    >
+                        Go Home
+                    </Button>
+                )}
+                {isDeleting && (
+                    <>
+                        <Button size='medium' className='width-full mt-4' isLoading disabled>
+                            Deleting Candy Machine... <Spinner size='small' />
+                        </Button>
+                    </>
+                )}
+            </div>
         </>
     )
 }
