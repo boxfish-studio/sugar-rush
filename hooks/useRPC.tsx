1085f3eeceda5a916fdf04066fdef1ab48d6e2a6
cpl121 <cpeon@boxfish.studio>
2022-07-22 14:26:08 +0200

    set rpc state the network connection

--- hooks/useRPC.tsx
+++ hooks/useRPC.tsx
@@ -1,11 +1,22 @@
 import { useRecoilState } from 'recoil'
 import { networkState } from 'lib/recoil-store/atoms'
+import { Connection } from '@solana/web3.js'
+import { useEffect, useState } from 'react'
+
+// const RPC_API_MAINNET = process.env.NEXT_PUBLIC_RPC_API_MAINNET as string
 
 const useRPC = () => {
     const [network] = useRecoilState(networkState)
+    const [rpc, setRpc] = useState<Connection>()
+
+    useEffect(() => {
+        if (network?.connection) {
+            setRpc(network.connection)
+        }
+    }, [network])
 
     return {
-        connection: network?.connection,
+        connection: rpc,
         isDevnet: network?.url.includes('devnet') ? '?cluster=devnet' : '',
     }
 }
