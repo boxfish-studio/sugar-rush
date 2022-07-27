48d4d85508dfbeb5faf313f54b02ad36f0275083
cpl121 <cpeon@boxfish.studio>
2022-07-27 17:50:50 +0200

    Remove topActions component

--- components/TopActions.tsx
+++
@@ -1,7 +0,0 @@
-import { FC } from 'react'
-
-const TopActions: FC<{ children: any }> = ({ children }) => {
-    return <div className='d-flex flex-justify-end column-gap-1 flex-row flex-md-row'>{children}</div>
-}
-
-export default TopActions
