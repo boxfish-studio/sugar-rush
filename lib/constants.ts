fe137470a9764dcecee8d5c397d643e6aba9a798
Begoña Alvarez <balvarez@boxfish.studio>
2022-08-03 14:15:21 +0200

    feat: decrease notification default timeout

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,4 +1,4 @@
 export const JSON_EXTENSION = 'application/json'
 export const DEFAULT_TIMEOUT = 30000
 export const NOTIFICATION_TIMEOUT_NEVER = -1
-export const NOTIFICATION_TIMEOUT_DEFAULT = 9000
+export const NOTIFICATION_TIMEOUT_DEFAULT = 5000
