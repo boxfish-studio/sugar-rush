42b58fd1e31ed353ca42b1ac118034fce0853448
Arnau Espin <arnauespin@gmail.com>
2022-08-16 11:46:07 +0200

    fix: remove unnecessary import

--- components/candy-machine/UpdateCandyMachine.tsx
+++ components/candy-machine/UpdateCandyMachine.tsx
@@ -1,6 +1,6 @@
 import { AnchorProvider, BN, Program } from '@project-serum/anchor'
 import { useAnchorWallet, useWallet } from '@solana/wallet-adapter-react'
-import { LAMPORTS_PER_SOL, PublicKey, Connection } from '@solana/web3.js'
+import { LAMPORTS_PER_SOL, PublicKey } from '@solana/web3.js'
 import { useForm, useRPC, useUploadCache } from 'hooks'
 import { updateV2 } from 'lib/candy-machine'
 import { CANDY_MACHINE_PROGRAM_V2_ID, DEFAULT_GATEKEEPER } from 'lib/candy-machine/constants'
