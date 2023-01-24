b58f76a4b6b820dda5d1ec23a23d0c79c76f954a
Arnau Espin <arnauespin@gmail.com>
2023-01-24 16:42:42 +0100

    feat: create own wallet wrapper

--- components/Wallet.tsx
+++ components/Wallet.tsx
@@ -1,10 +1,19 @@
-import { WalletMultiButton } from '@solana/wallet-adapter-react-ui'
+import { useWallet } from '@solana/wallet-adapter-react'
+import { WalletMultiButton } from 'components/WalletWrapper'
 import { FC } from 'react'
 
-const Wallet: FC = () => (
-    <div className='position-fixed right-0 scale-75 flex flex-row gap-3 z-10'>
-        <WalletMultiButton />
-    </div>
-)
+const Wallet: FC = () => {
+    const { publicKey } = useWallet()
+
+    return (
+        <div className='position-fixed right-0 scale-75 flex flex-row gap-3 z-10'>
+            <WalletMultiButton className='solana-wallet-adapter-button'>
+                {!publicKey
+                    ? 'Connect Wallet'
+                    : publicKey?.toBase58().slice(0, 4) + '..' + publicKey?.toBase58().slice(-4)}{' '}
+            </WalletMultiButton>
+        </div>
+    )
+}
 
 export default Wallet
